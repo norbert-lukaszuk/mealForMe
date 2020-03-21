2d3b88463c17e902a723c84b608873e7e7614313
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-03-21 14:21:06 +0100

    serch phrase getting

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const app__name = document.getElementById('app__name');
+const form = document.getElementById('form');
+const resoult__wraper = document.getElementById('resoult__wraper');
+
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    const search = form.meal__search.value;
+    form.reset();
+    const paragraph = `<p>${search}</p>`;
+    resoult__wraper.innerHTML += paragraph;
+})

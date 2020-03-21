582cc7229d177cd294dfdac3c9a10c57f53f7570
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-03-21 22:40:54 +0100

    mealdb api connecting

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const app__name = document.getElementById('app__name');
 const form = document.getElementById('form');
 const resoult__wraper = document.getElementById('resoult__wraper');
+const random__button = document.getElementById('random__button');
 
 form.addEventListener('submit',e=>{
     e.preventDefault();
     const search = form.meal__search.value;
     form.reset();
-    const paragraph = `<p>${search}</p>`;
-    resoult__wraper.innerHTML += paragraph;
+    resoult__wraper.innerHTML = '';
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+    .then(res=>res.json())
+    .then(data=>{
+        data.meals.forEach(e=>{
+            console.log(e)
+            const paragraph = `<p>${e.strMeal}</p>`;
+            resoult__wraper.innerHTML += paragraph;
+            resoult__wraper.innerHTML += `<img class="thumbnail" src="${e.strMealThumb}" alt="${e.strMeal}" />`;
+        })
+    });
 })
+random__button.addEventListener('click', e=>{
+    e.preventDefault();
+    resoult__wraper.innerHTML = '';
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res=>res.json())
+    .then(data=>{
+        
+        // data.meals.forEach(e=>{
+            console.log(data.meals[0])
+            const paragraph = `<p>${data.meals[0].strMeal}</p>`;
+            resoult__wraper.innerHTML += paragraph;
+            resoult__wraper.innerHTML += `<img class="thumbnail" src="${data.meals[0].strMealThumb}" alt="${data.meals[0].strMeal}" />`;
+        })
+    });
+

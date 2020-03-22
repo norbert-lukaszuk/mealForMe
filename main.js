97b718810dabf00c5dd66a10940fc8d74f2da568
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-03-22 22:29:12 +0100

    adding second api for search

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app__name = document.getElementById('app__name');
 const form = document.getElementById('form');
 const resoult__wraper = document.getElementById('resoult__wraper');
 const random__button = document.getElementById('random__button');
+const apiKey = '8125b5fc0f034ef1869347654bb52146';
 
 form.addEventListener('submit',e=>{
     e.preventDefault();
@@ -17,6 +18,24 @@ form.addEventListener('submit',e=>{
             resoult__wraper.innerHTML += paragraph;
             resoult__wraper.innerHTML += `<img class="thumbnail" src="${e.strMealThumb}" alt="${e.strMeal}" />`;
             resoult__wraper.innerHTML += `<a class="link" href="${e.strSource}">Link<a>`;
+        })
+    });
+    fetch(`https://api.spoonacular.com/recipes/search?query=${search}&number=10&information&instructionsRequired=true&apiKey=${apiKey}`)
+    .then(res=>res.json())
+    .then(data=>{
+        data.results.forEach(e=>{
+            console.log(e)
+            const paragraph = `<p>${e.title}</p>`;
+            resoult__wraper.innerHTML += paragraph;
+            resoult__wraper.innerHTML += `<img class="thumbnail" src="https://spoonacular.com/recipeImages/${e.id}-480x360.jpg" alt="${e.title}" />`;
+            // resoult__wraper.innerHTML += `<a class="link" href="${e.strSource}">Link<a>`;
+            fetch(`https://api.spoonacular.com/recipes/${e.id}/analyzedInstructions?apiKey=${apiKey}`)
+            .then(res=>res.json())
+            .then(data=>{
+                data.forEach(steps=>{
+                    steps.steps.forEach(step=>console.log(step))
+                })
+            })
         })
     });
 })
